6959a56a2bfa1830a2e917d7797cac23ea47cedd
Lachezar Naydenov <l.naidenov@slicecrowd.com>
2023-12-13 10:27:42 +0200

    [test quick form]

--- wp-content/themes/kenduro/patterns/atoms/svg.php
+++ wp-content/themes/kenduro/patterns/atoms/svg.php
@@ -1,14 +1,12 @@
 <?php
 	$defaults = [
-		'name'  => '',
-		'class' => '',
-		'url'   => null,
+	  'name' => '',
+	  'class' => '',
 	];
+	
+	$args = wp_parse_args( $args, $defaults );
+	$class = trim( implode( ' ', (array) $args['class'] ) );
+	$class = ($class !== '') ? ' ' . $class : '';
 
-	$args     = wp_parse_args( $args, $defaults );
-	$site_url = get_site_url();
-	$url      = ( null === $args['url'] ) ? esc_url( ICON_PATH . '/' . $args['name'] . '.svg' ) : $url = str_replace( $site_url, '.', $args['url'] );
-	$class    = trim( implode( ' ', (array) $args['class'] ) );
-	$class    = ( '' !== $class ) ? ' ' . $class : '';
-
-	echo '<i class="svg' . esc_attr( $class ) . '">' . wp_kses( wp_remote_get( esc_url( $url ) ) ) . '</i>';
+	echo file_get_contents(ICON_PATH . '/' . $args['name'] . '.svg');
+?>
