df25a0cd48173aaae7101debab04051220a68c30
Lachezar Naydenov <l.naidenov@slicecrowd.com>
2023-12-14 13:36:52 +0200

    [homepage design]

--- wp-content/themes/kenduro/patterns/organisms/homepage/category-pills/index.php
+++ wp-content/themes/kenduro/patterns/organisms/homepage/category-pills/index.php
@@ -1,23 +1,51 @@
-<ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
-  <li class="nav-item" role="presentation">
-    <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">All</button>
-  </li>
-  <li class="nav-item" role="presentation">
-    <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Goggles</button>
-  </li>
-  <li class="nav-item" role="presentation">
-    <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Helmets</button>
-  </li>
-</ul>
-<hr>
-<div class="tab-content" id="pills-tabContent">
-  <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
-    <?php echo do_shortcode("[products limit='10']"); ?>
+<?php 
+use Lean\Load;
+$args = array(
+  'taxonomy' => 'product_cat',
+  'parent' => 0,
+  'hide_empty' => true
+);
+$main_categories = get_terms($args);
+
+
+if ($main_categories) : ?>
+
+  <ul class="nav nav-pills" id="pills-tab" role="tablist">
+    <li class="nav-item" role="presentation">
+      <button class="nav-link active" id="pills-all-tab" data-bs-toggle="pill" data-bs-target="#pills-all" type="button" role="tab" aria-controls="pills-all" aria-selected="true">All</button>
+    </li>
+    <?php foreach ($main_categories as $main_category) :
+      // pretty_dump($main_category);
+      $slug = $main_category->slug;
+    ?>
+      <li class="nav-item" role="presentation">
+        <button class="nav-link" id="pills-<?php echo $slug; ?>-tab" data-bs-toggle="pill" data-bs-target="#pills-<?php echo $slug; ?>" type="button" role="tab" aria-controls="pills-<?php echo $slug; ?>" aria-selected="false"><?php echo $main_category->name; ?></button>
+      </li>
+    <?php 
+      endforeach;
+      Load::atoms('link/index', [
+        'text' => 'Browse All Products',
+        'class' => 'underline',
+        'url' => '#',
+        'icon' => 'arrow_down'
+      ]); 
+    ?>
+  </ul>
+  <hr>
+  <div class="tab-content" id="pills-tabContent">
+    <div class="tab-pane fade show active" id="pills-all" role="tabpanel" aria-labelledby="pills-all-tab">
+    <!-- ADD PAGINATION 
+    [products limit='10' paginate='true'] 
+    ================================= -->
+      <?php echo do_shortcode("[products limit='10']"); ?> 
+    </div>
+    <?php foreach ($main_categories as $main_category) :
+      $id = $main_category->term_id;
+      $slug = $main_category->slug;
+    ?>
+      <div class="tab-pane fade" id="pills-<?php echo $slug; ?>" role="tabpanel" aria-labelledby="pills-<?php echo $slug; ?>-tab">
+        <?php echo do_shortcode("[products category='".$id."' limit='10']"); ?>
+      </div>
+    <?php endforeach; ?>
   </div>
-  <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
-    <?php echo do_shortcode("[products category='1937']"); ?>
-  </div>
-  <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">
-    <?php echo do_shortcode("[products category='1941']"); ?>
-  </div>
-</div>
+<?php endif; ?>
