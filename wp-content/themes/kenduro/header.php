79c36b6d1f2e1ec9eb0a04ded7cd0f52cd366bad
Lachezar Naydenov <l.naidenov@slicecrowd.com>
2024-03-22 09:10:45 +0200

    [Added dsk leasing]

--- wp-content/themes/kenduro/header.php
+++ wp-content/themes/kenduro/header.php
@@ -8,8 +8,6 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=0">
 	<meta name="facebook-domain-verification" content="959mi9u8mxe95xoet9djmp9rux5q97" />
 	
-	<title><?php wp_title(); ?></title>
-
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="baseurl" href="<?php echo esc_url(get_site_url()); ?>">
 
--- wp-content/themes/kenduro/single.php
+++ wp-content/themes/kenduro/single.php
@@ -1,18 +1,93 @@
 <?php
+
 /**
  * The template for displaying all single posts
  */
 
 get_header();
+$url = $_SERVER['REDIRECT_URL'];
+$is_dsk = $url === '/dsk/' || $url === '/dskbuy' || $url === '/dsksend';
+$dsk_classes_cols = $is_dsk ? 'col-xl-8 offset-xl-2' : '';
 
-if ( have_posts() ) :
-	/* Start the Loop */
-	while ( have_posts() ) :
-		the_post();
+$dsk_classes = '';
+switch ($url) {
+	case '/dsk/':
+		$dsk_classes = 'step-1';
+		break;
 
-		/* Post content here */
+	case '/dskbuy':
+		$dsk_classes = 'step-2';
+		break;
 
-	endwhile; // End of the loop.
-endif;
+	case '/dsksend':
+		$dsk_classes = 'step-3';
+		break;
 
-get_footer();
+	default:
+		# code...
+		break;
+}
+
+if ($is_dsk) :
+?>
+	<div class="dsk-page <?php echo esc_attr($dsk_classes); ?>" id="primary">
+	<?php endif; ?>
+
+	<div class="container">
+		<div class="row">
+			<div class="col <?php echo esc_attr($dsk_classes_cols); ?>">
+				<?php
+				if (have_posts()) :
+					/* Start the Loop */
+					while (have_posts()) :
+						the_post();
+
+						if ($is_dsk) : ?>
+								<ul class="dsk-steps">
+									<li class="dsk-steps__item finance-calculator">
+										<span class="circle"></span>
+										<p class="paragraph paragraph-l">Финансови условия</p>
+									</li>
+									<li class="dsk-steps__item private-data">
+										<span class="circle"></span>
+										<p class="paragraph paragraph-l">Лични Данни</p>
+									</li>
+									<li class="dsk-steps__item request-sent">
+										<span class="circle"></span>
+										<p class="paragraph paragraph-l">Успешно изпратена заявка</p>
+									</li>
+								</ul>
+							<?php
+						endif;
+
+						if ($url === '/dsksend') :
+							?>
+								<div class="dsk-sent">
+								<?php
+							endif;
+
+							the_content();
+
+							if ($url === '/dsksend') :
+							?>
+							<a href="<?php echo esc_attr(get_site_url()); ?>/shop" class="button button-primary-orange paragraph paragraph-m semibold">
+								<span>Върни се към Kenduro</span>
+							</a>
+							</div>
+				<?php endif;
+						/* Page content here */
+
+						endwhile; // End of the loop.
+					endif;
+				?>
+
+			</div>
+		</div>
+	</div>
+	<?php
+	if ($is_dsk) :
+	?>
+	</div>
+<?php endif;
+
+	get_footer();
