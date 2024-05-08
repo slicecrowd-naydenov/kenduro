51c92bae14f9f47c3f2387b6d662d5ba0c9e2a81
Lachezar Naydenov <l.naidenov@slicecrowd.com>
2024-05-08 11:06:38 +0300

    [on-sale with filters]

--- wp-content/themes/kenduro/templates/on-sale-filter.php
+++ wp-content/themes/kenduro/templates/on-sale-filter.php
@@ -25,7 +25,7 @@ get_header();
 							// $list_categories($taxonomies, array());
 						?>
 						<p class="paragraph paragraph-xl semibold cat-head active-cat filters">Филтри</p>
-						<?php echo do_shortcode('[wpf-filters id=1]'); ?>
+						<?php echo do_shortcode('[wpf-filters id=2]'); ?>
 	
 					</div>
           <?php echo do_shortcode('[awdr_sale_items_list columns="4" per_page="20"]'); ?>
