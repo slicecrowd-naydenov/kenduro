419d2a885389b57a33a3211091679b3f952a6dc0
Lachezar Naydenov <l.naidenov@slicecrowd.com>
2024-05-08 11:00:21 +0300

    [on-sale with filters]

--- wp-content/themes/kenduro/templates/on-sale-filter.php
+++ wp-content/themes/kenduro/templates/on-sale-filter.php
@@ -20,39 +20,14 @@ get_header();
           ]);
         ?>
 				<div class="filter-content-wrapper">
-          <div class="mobile-wrapper filter-sidebar">
-            <?php
-              if (wp_is_mobile()) {
-                ?>
-                <!-- Button trigger modal -->
-                <button type="button" class="button filter-modal" data-toggle="modal" data-target="#filterModal">
-                  Филтри
-                </button>
-
-                <!-- Modal -->
-                <div class="modal fade mobile-modal" id="filterModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-                  <div class="modal-dialog modal-dialog-centered" role="document">
-                    <div class="modal-content">
-                      <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLongTitle">Филтри</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                          <?php Load::atom('svg', ['name' => 'close']); ?>
-                        </button>
-                      </div>
-                      <div class="modal-body">
-                        <?php
-                          Load::molecules('product-category/product-categories-filter/index');
-                        ?>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <?php
-              } else {
-                Load::molecules('product-category/product-categories-filter/index');
-              }
-            ?>
-          </div>
+          <div class="filter-sidebar">
+						<?php
+							// $list_categories($taxonomies, array());
+						?>
+						<p class="paragraph paragraph-xl semibold cat-head active-cat filters">Филтри</p>
+						<?php echo do_shortcode('[wpf-filters id=1]'); ?>
+	
+					</div>
           <?php echo do_shortcode('[awdr_sale_items_list columns="5" per_page="20"]'); ?>
         </div>
       </div>
