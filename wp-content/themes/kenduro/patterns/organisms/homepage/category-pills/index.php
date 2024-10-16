1bafc17712ee146af4c037182578508d0d61712e
Lachezar Naydenov <l.naidenov@slicecrowd.com>
2024-10-16 09:12:04 +0300

    encoded image title

--- wp-content/themes/kenduro/patterns/organisms/homepage/category-pills/index.php
+++ wp-content/themes/kenduro/patterns/organisms/homepage/category-pills/index.php
@@ -55,7 +55,7 @@ if ($main_categories) : ?>
           ================================= -->
             <?php echo do_shortcode("[products limit='10' columns='5']"); ?>
             <a href="<?php echo esc_attr(get_site_url()); ?>/shop" class="cat-img">
-              <img src="<?php echo IMAGES_PATH; ?>/categories/Image=Grey, For=All Products.jpg" />
+              <img src="<?php echo IMAGES_PATH; ?>/categories/image_grey_for_all_products.jpg" />
               <h4>
                 Разгледайте <strong>всички</strong> Kenduro продукти
                 <?php Load::atom('svg', ['name' => 'arrow_xl']); ?>
