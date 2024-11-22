b64d98397fcd2e1a54a6db25084eba5b0138b131
Lachezar Naydenov <l.naidenov@slicecrowd.com>
2024-11-22 09:45:27 +0200

    [show Uxify only on live domain]

--- wp-content/themes/kenduro/header.php
+++ wp-content/themes/kenduro/header.php
@@ -4,15 +4,17 @@
 <html <?php language_attributes(); ?> class="no-js no-svg">
 
 <head>
-	<script>
-   	(() => {
-      let t = document.createElement("script");
-      t.src = "https://businesswebvitals.com/YaNLaxvZfkjmdLVu"
-      t.async = !0;
-      t.id = "uxify-script";
-      document.head.appendChild(t);
-   	})();
-	</script>
+	<?php if (isset($_SERVER['HTTP_HOST']) && in_array($_SERVER['HTTP_HOST'], ['kenduro.com', 'www.kenduro.com'])) : ?>
+		<script>
+			(() => {
+				let t = document.createElement("script");
+				t.src = "https://businesswebvitals.com/YaNLaxvZfkjmdLVu"
+				t.async = !0;
+				t.id = "uxify-script";
+				document.head.appendChild(t);
+			})();
+		</script>
+	<?php endif; ?>
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<!-- <meta name="facebook-domain-verification" content="959mi9u8mxe95xoet9djmp9rux5q97" /> -->
