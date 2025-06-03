867c2964b6d3485373c3dd3f0f46fc2a174d0712
Lachezar Naydenov <l.naidenov@slicecrowd.com>
2025-06-03 15:28:25 +0300

    [change h1 text]

--- wp-content/themes/kenduro/templates/homepage.php
+++ wp-content/themes/kenduro/templates/homepage.php
@@ -7,7 +7,7 @@ get_header();
 
 ?>
 <main id="primary">
-  <h1 class="hidden-h1">Kenduro</h1>
+  <h1 class="hidden-h1">Mагазин за Mотори - Kenduro</h1>
   <?php 
     Load::organisms('homepage/hero-section/index');
     Load::organisms('information-list/index', [
